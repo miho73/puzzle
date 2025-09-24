a42af5dc613289867f3c2c0c0be570d13eb3585e
HYUN CHANGWOON <hyrexfiels@gmail.com>
2025-09-24 15:34:13 +0900

    SVG update

--- src/assets/royal/Svg.tsx
+++ src/assets/royal/Svg.tsx
@@ -1,3 +1,7 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-nocheck
+// Importing SVG file works fine somehow
+
 import RoyalC1 from './royal_symbol_c1.svg?react';
 import RoyalC2 from './royal_symbol_c2.svg?react';
 import RoyalG39 from './royal_symbol_g39.svg?react';
